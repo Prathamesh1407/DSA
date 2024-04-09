e3c8d2352889a6bc6b847ee426ca68f9962d592f
Prathamesh Santosh Thorat <128747798+Prathamesh1407@users.noreply.github.com>
2024-04-09 17:48:40 +0530

    Traversal Done

---
+++ BinaryTree/TopView.cpp
@@ -0,0 +1,31 @@
+/*
+    class Solution {
+  public:
+    vector <int> topView(Node *root) {
+        // Your Code Here
+        if(root==NULL) return {};
+        map<int,int>m;
+        queue<pair<Node*,int>>q;
+        q.push(make_pair(root,0));
+        vector<int>v;
+        while(!q.empty())
+        {
+            pair<Node*,int>temp=q.front();
+            q.pop();
+            Node* frontNode=temp.first;
+            int hd=temp.second;
+            
+            if(m.find(hd)==m.end()) m[hd]=frontNode->data;
+            
+            if(frontNode->left) q.push(make_pair(frontNode->left,hd-1));
+            if(frontNode->right) q.push(make_pair(frontNode->right,hd+1));
+        }
+        
+        for(auto val:m) 
+        {
+            v.push_back(val.second);
+        }
+        return v;
+    }
+};
+*/
